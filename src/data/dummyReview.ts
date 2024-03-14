62b3e79cf3020b2745b40351eedb2cce28b64463
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-14 10:55:49 +0800

    Update dummyReview.ts with new reviews

--- src/data/dummyReview.ts
+++ src/data/dummyReview.ts
@@ -3,21 +3,21 @@ export const review = [
     id: 1,
     name: 'Jonathan Doe',
     review:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at tincidunt libero. Nulla facilisi. Sed nec libero et libero vehicula lacinia. Nulla facilisi. Sed nec libero et libero vehicula lacinia.',
+      'I had a great experience with this product. It was easy to use and met all of my expectations. I would definitely recommend it to others.',
     rating: 4,
   },
   {
     id: 2,
     name: 'Josiah Doughnut',
     review:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at tincidunt libero. Nulla facilisi. Sed nec libero et libero vehicula lacinia. Nulla facilisi. Sed nec libero et libero vehicula lacinia.',
+      'The product was okay. It did what it was supposed to do, but I was not overly impressed. I think there are other products out there that could do a better job.',
     rating: 3,
   },
   {
     id: 3,
     name: 'Rusty Shackleford',
     review:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at tincidunt libero. Nulla facilisi. Sed nec libero et libero vehicula lacinia. Nulla facilisi. Sed nec libero et libero vehicula lacinia.',
+      'This is the best product I have ever used! It exceeded all of my expectations and I am extremely satisfied. I will definitely be purchasing this product again in the future.',
     rating: 5,
   },
 ];
