879e1e37bbb994162d3d1eea048c971f21d540d9
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-14 10:55:06 +0800

    Update Footer component to use react-router-dom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,19 @@
 import { Facebook, Twitter, Youtube } from 'react-feather';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
     <footer className='footer footer-center p-10 bg-slate-200 text-base-content rounded'>
       <nav className='grid grid-flow-col gap-4'>
-        <a className='link link-hover'>About us</a>
-        <a className='link link-hover'>Contact</a>
-        <a className='link link-hover'>Partners</a>
-        <a className='link link-hover'>Blog</a>
+        <Link to='/' className='link link-hover'>
+          About
+        </Link>
+        <Link to='/store' className='link link-hover'>
+          Store
+        </Link>
+        <Link to='/collection' className='link link-hover'>
+          Collection
+        </Link>
       </nav>
       <nav>
         <div className='grid grid-flow-col gap-4'>
