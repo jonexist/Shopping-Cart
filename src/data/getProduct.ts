d9a1c6580a3ef818dd0f93e552aed1d92666be2a
Jonexist <79012225+jonexist@users.noreply.github.com>
2024-03-14 08:57:20 +0800

    Refactor getProduct function to handle errors and define Product type

--- src/data/getProduct.ts
+++ src/data/getProduct.ts
@@ -1,10 +1,25 @@
-export const getProduct = async () => {
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  category: string;
+  readonly rating: {
+    rate: number;
+    count: number;
+  };
+};
+
+export const getProduct = async (): Promise<Product[] | []> => {
   try {
     const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: Product[] = await response.json();
     return data;
   } catch (error) {
-    console.log('error', error);
-    return null;
+    console.error('Failed to fetch products: ', error);
+    return [];
   }
 };
